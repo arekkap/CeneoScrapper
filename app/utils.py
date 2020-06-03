92b8c84b8d324e1b2c4e7d28b8742b06664325f5
Arkadiusz Kapron <arekk99@gmail.com>
2020-06-03 12:06:07 +0200

    AK 27.05

--- app/utils.py
+++ app/utils.py
@@ -21,8 +21,8 @@ def remove_whitespaces(text):
 def extract_feature(opinion, selector, attribute = None):
     try:
         if not attribute:
-            return opinion.select(selector).pop().get_text().strip()
+            return opinion.select(selector).pop(0).get_text().strip()
         else:
-            return opinion.select(selector).pop()[attribute]
+            return opinion.select(selector).pop(0)[attribute]
     except IndexError:
         return None
